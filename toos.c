35b3b761923b925b671d0edc503fce7ad304e5ed
yves <ty@msn.cn>
2015-03-30 23:02:24 +0800

    add toos.c

---
+++ toos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Asicc2Hex(char* src, char* result,size_t length)
+{
+    return 0;
+}
+
+int Hex2Asicc(char* src, char* result,size_t length)
+{
+    return 0;
+}
+
